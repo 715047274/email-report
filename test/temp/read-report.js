81645f48a829225caf4df09d0248104d39c358b9
Kevin Zhang <K.Zhang@ceridian.com>
2024-06-21 00:17:44 -0400

    [sq] update db

--- test/temp/read-report.js
+++ test/temp/read-report.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const bjson = require('big-json');
-const fileLocation = path.join(__dirname, 'data','report_sample.json')
+const fileLocation = path.join(__dirname,'..' ,'data','report_sample2.json')
 const fileLocationRemote = 'http://nan4dfc1tst04.custadds.com:8080/job/sanity-test-payroll-ui/94/execution/node/3/ws/cypress/reports/index.json'
 const readStream = fs.createReadStream(fileLocation) // => readStream);
 const parseStream = bjson.createParseStream();
